b0a0e197fd59e8e172f83c794e43d3ba8e021240
hanabahmi <brahmihana7@gmail.com>
2024-04-19 12:37:52 +0200

    update

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,5 +1,12 @@
+import jwt from "jsonwebtoken"
 export const shouldBeLoggedIn = async (req,res) => {
     const token = req.cookies.token 
+    if(!token)
+        return res.status(401).json({message: "Noy Authnticated!"});
+    jwt.verify(token, process.env.JWT_SECRET_Key , async (err, payload) => {
+        if (err) res.status(403).json({message: "Token is not Valid !"});
+    })
+    res.status(200).json({message : "You are Authenticated"});
 }
 export const shouldBeAdmin = async (req, res ) => {
 
